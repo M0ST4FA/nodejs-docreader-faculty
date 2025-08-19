5801d51b55ba1a8cf8feeaa52d5d8c811cbc95dc
M0sT4fa <mostafa2018a3s@gmail.com>
2025-08-19 22:30:54 +0200

    fix(httpOnly)

--- utils/JWTService.ts
+++ utils/JWTService.ts
@@ -34,7 +34,11 @@ class JWTService {
       httpOnly: true,
     };
 
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    if (process.env.NODE_ENV === 'production') {
+      cookieOptions.domain = process.env.COOKIE_DOMAIN;
+      cookieOptions.path = '/';
+      cookieOptions.secure = true;
+    }
 
     res.cookie('jwt', token, cookieOptions);
 
