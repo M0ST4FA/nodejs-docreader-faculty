93e0ad791a3d2070d4ad82d4a0aecb47dfb93a7b
M0sT4fa <mostafa2018a3s@gmail.com>
2025-08-25 09:38:14 +0200

    feat(topic): add seeding script for existing years

--- scripts/seedTopics.ts
+++ scripts/seedTopics.ts
@@ -4,35 +4,42 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const faculties = await prisma.faculty.findMany({
-    include: { years: true },
+  const years = prisma.studyingYear.findMany({
+    where: {},
+    include: { faculty: true },
   });
 
-  for (const faculty of faculties) {
-    for (const year of faculty.years) {
-      const topicName = `faculty_${faculty.id}_year_${year.id}`;
-      const description = `Topic for notifications of faculty ${faculty.name} and year with Id ${year.title}`;
+  console.log('⚙️ Fetching years...');
 
-      // Check if topic already exists
-      const exists = await prisma.topic.findUnique({
-        where: { name: topicName },
-      });
+  const promises = (await years).map(async year => {
+    const topicName = year.id.toString();
+    const topicDescription = `Topic for notifications of year ${year.title} of faculty ${year.faculty.name}.`;
 
-      if (!exists) {
-        await prisma.topic.create({
-          data: {
-            name: topicName,
-            description,
-            public: true, // optional, depends on your design
-            creatorId: null, // or system user id if you track this
-          },
-        });
-        console.log(`✅ Created topic: ${topicName}`);
-      } else {
-        console.log(`ℹ️ Skipped (already exists): ${topicName}`);
-      }
-    }
-  }
+    const topic = await prisma.topic.upsert({
+      create: { name: topicName, description: topicDescription },
+      update: {},
+      where: { name: topicName },
+    });
+
+    await prisma.studyingYear.update({
+      where: { id: year.id },
+      data: { topicId: topic.id },
+    });
+
+    return topic;
+  });
+
+  console.log('✅ Fetched years successfully.');
+
+  console.log('⚙️ Inserting topics...');
+
+  await Promise.all(promises).then(topics =>
+    topics.forEach(topic =>
+      console.log(
+        `✅ Successfully inserted topic with name ${topic.name} or it already existed.`,
+      ),
+    ),
+  );
 }
 
 main()
